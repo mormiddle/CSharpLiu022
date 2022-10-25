9f0663a19e7efc278de97fc114c79f84992bddd5
mormiddle <mormiddle@163.com>
2022-10-25 10:20:36 +0800

    c#刘铁猛022事件详解（下）

---
+++ EventExample/EventExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Customer
+    {
+        public double Bill { get; set; }
+        public void PlayTheBill()
+        {
+            Console.WriteLine("I will play ${0}",this.Bill);
+        }
+    }
+}
